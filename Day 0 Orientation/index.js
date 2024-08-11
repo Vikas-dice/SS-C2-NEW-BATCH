3b9dfa81d5225d7cdbb70d0603b9bdc84db6caa5
Vikas Chaudhary DICE <vikas@dice-academy.com>
2024-08-11 13:03:48 +0530

    code pushed for js day 0-orientation

---
+++ Day 0 Orientation/index.js
@@ -0,0 +1,250 @@
+/*
+multiline comment
+
+output 
+variables -input
+
+
+
+*/
+//simgle line comment 
+
+// console.log("vikas chaudhary");
+// console.log("dice academy");
+// process.stdout.write("full stack developer");
+// process.stdout.write("mern stack");
+
+// console.log("hey"); console.log("bye");
+
+/*
+variables - container - bucket 
+
+
+*/
+
+// let age=27;
+// let name="vikas";
+// let ismarried=false;
+// let salary=10000;
+
+// console.log(age);
+// console.log(name);
+// console.log(ismarried);
+// console.log(salary);
+
+// process.stdout.write(name);
+
+
+
+
+
+
+
+
+/*
+naming convention
+
+
+
+*/
+
+// let Name="vikas";
+// let name="vikas";
+// let name1="vikas";
+// // let 1name="vikas";
+// let .name="vikas";
+// let name.="vikas";
+// let $name="vikas";
+// let name$="vikas";
+// let #name="vikas";
+// let name#="vikas";
+// let @name="vikas";
+
+// let na1me="vikas";
+// let na$me="vikas";
+// let na_me="vikas";
+// let na@me="vikas";
+
+
+// let let="vikas";
+// let var="vikas";
+// let continue="vikas";
+// let goto="vikas";
+// let if="vikas";
+// let break="vikas";
+
+
+// let a=27;
+
+
+/*
+
+maths - arithmatics operator
+relational oper - rel - true/false
+>
+>
+>=
+>=
+==
+!=
++
+-
+*
+/
+%
+
+*/
+
+// console.log(10+5);
+// console.log(10-5);
+// console.log(10*5);
+// console.log(11/5);
+// console.log(11%5);
+
+// console.log(Math.floor(11/5));
+// console.log(Math.ceil(11/5));
+
+
+
+
+
+// console.log(10>5)
+// console.log(10<5);
+// console.log(10==5);
+// console.log(10!=5);
+// console.log(10>=5);
+// console.log(10>=10);
+// console.log(10<=5);
+// console.log(10<=10);
+
+
+
+
+
+
+/*
+10>5 true
+10>10 false
+10=10 true
+10<5 false
+10=5 false
+10<10 false
+10=10 true
+
+
+
+
+*/
+
+
+
+
+
+
+
+
+// console.log(10>5);
+// console.log(10<5);
+// console.log(10==5);
+// console.log(10==10);
+// console.log(10!=6);
+// console.log(10>=8);
+// console.log(10<=8);
+// console.log(10>=10);
+// console.log(10<=20);
+
+
+
+/*
+10<20 true
+10>10 false
+10=10 true
+10<8 false
+10=8 false
+
+
+10>8 true
+
+
+*/
+
+/*
+logical operators - and or not 
+
+and - hindi wala guna  &&
+or = hindi wala jodna  ||
+not !
+
+
+
+*/
+
+
+
+
+// console.log(true && true );
+// console.log(true && false);
+// console.log(false && false);
+// console.log(false && true);
+
+
+
+
+
+/*
+0&&1
+0*1=0=false
+0&&0
+0 * 0=0 =false
+1 && 0
+1*0=0 = false
+
+
+1 && 1
+1*1=1 = true
+
+*/
+
+
+
+
+
+
+console.log(true  || true);
+console.log(true || false);
+console.log(false || false);
+console.log(false || true);
+
+
+
+/*
+1||0
+1+0=1=true
+1 || 1
+1+1=2= non zero= true
+
+*/
+
+
+//not !
+
+console.log(!true);
+console.log(!false);
+
+console.log(true || false && true);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

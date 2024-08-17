fad639c82fb24da8a9b0a927446ae5511be12491
Vikas Chaudhary DICE <vikas@dice-academy.com>
2024-08-17 12:35:47 +0530

    js day 1 class 2

--- Day 0 Orientation/index.js
+++ Day 0 Orientation/index.js
@@ -248,3 +248,4 @@ console.log(true || false && true);
 
 
 
+
---
+++ Day 01/01Conditionals.js
@@ -0,0 +1,235 @@
+
+
+// syntax 
+
+// if(statement){
+
+// }
+
+
+// let age=19; //ram=age=19
+
+
+// if(age>18){
+//     console.log("you can vote");
+// }
+// console.log("vikas")
+
+/*
+l10 age =19= ram
+l13 age>18 19>18 true 16>18 false l16
+l14 you can vote
+
+l10 age=19
+l13 age>18 19/,18 true
+l14 you can vote
+l16 vikas
+
+
+
+multiple if ==
+*/
+
+
+
+
+// let age=20;
+
+// if(age>18){
+//     console.log("you can vote");
+    
+// }
+
+// if(age==18){
+//     console.log("you can also vote");
+    
+// }
+
+// if(age<18){
+//     console.log("you cant vote ");
+    
+// }
+
+// console.log("dice academy");
+
+
+/*
+l36 ram=age=20
+l38 age>18 20>28 true
+l39 you  can vote
+l43 age==18 20==18 false
+l48 age<18 20<18 false
+
+
+
+*/
+
+/*
+if else 
+
+- else akela nahi aayga , ye hmesha if k sath aata h
+
+else{
+}
+
+
+*/
+// let umar=16;
+
+// if(umar>18){
+//     console.log("you can vote");
+    
+// }else{
+//     console.log("you cant vote");
+    
+// }
+/*
+l77 umar=20
+l79 umar>18 20>18 true
+l80 you can vote
+l77 umar =16
+l79 umar>18 16>18 false
+l83 you cant vote
+
+
+*/
+
+// let marks=90;
+// if(true){
+//     console.log("you are a topper");
+    
+// }else{
+//     console.log("go work hard ");
+    
+// }
+
+
+
+
+
+// let marks =48;
+
+// if(marks>=90){ //68>=90 false
+//     console.log("grade A");
+    
+// }else if(marks>80 && marks<90){ //68>=80
+//         console.log("grade B");
+        
+//     }else if(marks>70 && marks<80){ //68>70
+//             console.log("grade C");
+            
+//         }else if(marks>60 && marks<70){ //68>60 true
+//                 console.log("grade D");
+                
+//             }else if(marks>50 && marks<60){
+//                     console.log("grade E");
+                    
+//                 }else{
+//                     console.log("fail");
+//                 }
+            
+
+
+
+
+let age=20;
+
+switch(age){
+    case 18:
+        console.log("i am 18");
+        break;
+    case 19:
+        console.log("i am 19");
+        break;
+    case 20:
+        console.log("i am 20");
+        break;
+    case 21:
+        console.log("i am 21");
+        break;
+    case 22:
+        console.log("i am 22");
+        break;
+    default:
+        console.log("invalid age ");
+        
+        
+        
+        
+        
+        
+    
+}
+console.log("break ki vjah se yha agya m ");
+
+
+
+console.log("-------------------------------------------");
+
+
+let name="Vikas";
+switch(name){
+    case "puneet":
+        console.log("i am puneet");
+        break;
+    case "rohan":
+        console.log("i am rohan");
+        break;
+    case "vikas":
+        console.log("i am vikas");
+        break;
+    case "sahil":
+        console.log("i am sahil");
+        break;
+    case "dice":
+        console.log("i am dice");
+        break;
+    default:
+        console.log("invalid name");
+        
+
+        
+        
+        
+        
+        
+}
+
+console.log("00000000000000000000000000000000");
+
+
+switch("1"){
+    case 1:
+        console.log("i am one");
+        break;
+    case "dice":
+        console.log("i am dice");
+        break;
+    case 4:
+        console.log("i am four");
+        break;
+    case "4":
+        console.log("i am string 4");
+        break;
+    
+        
+        
+        
+        
+}
+
+
+console.log("----ternary operator------");
+
+
+// ?true :false
+
+
+let Age=37;
+
+Age>30?console.log("you are over 30"):console.log("you are under 30");
+
+
+
+
+
